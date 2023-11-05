c0d6f5cab51d652c5f0e7725abd489ec70606454
Redglaurung <43708381+Redglaurung@users.noreply.github.com>
2023-11-05 12:43:29 -0600

    Update rockstarGA.py

--- rockstarGA.py
+++ rockstarGA.py
@@ -6,11 +6,12 @@ import random
 class RockstarGA:
     # run the Genetic Algorithm with set specifications
 
-    def __init__(self, children, totalGenerations, childrenLength):
+    def __init__(self, children, totalGenerations, childrenLength, tuneLength):
         # initialization variables
         self.children = children
         self.totalGenerations = totalGenerations
         self.childrenLength = childrenLength
+        self.tuneLength = tuneLength
 
         # external calls setup
         self.rsg = RuleSet.RuleSetGenerator(self.childrenLength)
@@ -22,24 +23,38 @@ class RockstarGA:
 
 
     def generateStartingChildren(self):
-        #randomly generates a random set of starting seeds of size equal to childrenLength
+        # randomly generates a random set of starting seeds of size equal to childrenLength
         nextGeneration = [[]]
         newChild = []
         for x in range(self.children):
             for y in range(self.childrenLength):
                 newChild.append(random.choice(range(2)))
             nextGeneration.append(newChild)
-            print(newChild)
+            # print(newChild)
             newChild.clear()
         self.allGenerations.append(nextGeneration)
 
+    def generateCompleteTune(self, child):
+        # uses the attatched ruleset to calculate the automata's generation on a give string
+        childsTune = [[]]
+        childsTune.append(child)
+        for x in range(self.tuneLength-1):
+            nextString = self.rsg.step(child)
+            childsTune.append(nextString)
+        print(childsTune)
+        return childsTune
+
+    def determineChildDissonance(self, child):
+        # determines the dissonance of a given child by generation its tune, then running it through dsc
+        return self.dsc.determineTotalDissonance(self.generateCompleteTune(child))
 
     def start(self):
+        # begins the GA generation
         self.generateStartingChildren()
 
 
-def RunGA(children=20, totalGenerations=50, childrenLength =75):
-    ga = RockstarGA(children,totalGenerations,childrenLength)
+def RunGA(children=20, totalGenerations=50, childrenLength =75, tuneLength=50):
+    ga = RockstarGA(children,totalGenerations,childrenLength, tuneLength)
     ga.start()
 
 if __name__ == "__main__":
