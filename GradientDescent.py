8328e02dba3db75d7e3f3d622aa15199083426d0
Sniggiho <rohiggin@macalester.edu>
2023-11-21 13:58:03 -0600

    gradient descent works

--- GradientDescent.py
+++ GradientDescent.py
@@ -33,17 +33,18 @@ class HillClimber(object):
 
     def generateCompleteTune(self, child):
         # uses the attatched ruleset to calculate the automata's generation on a give string
-        childsTune = [[]]
+        childsTune = []
         childsTune.append(child)
         for x in range(self.tuneLength-1):
-            nextString = self.ruleSetGenerator.step(child)
+            nextString = self.ruleSetGenerator.step(childsTune[x])
             childsTune.append(nextString)
-        return childsTune[1:]
+        return childsTune
 
     def determineSeedDissonance(self, seed):
         # determines the dissonance of a given child by generation its tune, then running it through dissonanceCalculator
         # return self.dissonanceCalculator.determineTotalDissonance(self.generateCompleteTune(child))
         tune = self.generateCompleteTune(seed)
+        # print(tune[:3])
 
         scaleSteps = self.synth.majorPent
         numOcts = 5  # hardcoded for now
@@ -51,12 +52,12 @@ class HillClimber(object):
         notes = self.synth.interpretData1(tune, scale)
         return self.dissonanceCalculator.determineTotalDissonance(notes)
     
-    def determineDissonanceFromTune(self, tune):
-        scaleSteps = self.synth.majorPent
-        numOcts = 5  # hardcoded for now
-        scale = self.synth.getScale("A1", numOcts, scaleSteps)
-        notes = self.synth.interpretData1(tune, scale)
-        return self.dissonanceCalculator.determineTotalDissonance(notes)
+    # def determineDissonanceFromTune(self, tune):
+    #     scaleSteps = self.synth.majorPent
+    #     numOcts = 5  # hardcoded for now
+    #     scale = self.synth.getScale("A1", numOcts, scaleSteps)
+    #     notes = self.synth.interpretData1(tune, scale)
+    #     return self.dissonanceCalculator.determineTotalDissonance(notes)
     
     def getCurrValue(self):
         """Returns the dissonance of the current seed"""
@@ -100,31 +101,38 @@ class HillClimber(object):
     def getBestAllNeighs(self):
         """Looks at all neighbors, and returns the best (or the original state if all neighbors have a worse dissonance)"""
         neighbors = []
-        i=0
-        for char in self.currSeed:
-            if i == 0:
-                if char == "1":
-                    newSeed = "0" + self.currSeed[1:]
-                else:
-                    newSeed = "1" + self.currSeed[1:]
-                    
-            elif i == len(self.currSeed) - 1:
-                if char == "1":
-                    newSeed = self.currSeed[0:i] + "0"
-                else:
-                    newSeed = self.currSeed[0:i] + "1"
+        for i in range(len(self.currSeed)):
+            neighbor = list(self.currSeed)
+            if neighbor[i]:
+                neighbor[i]=0
             else:
-                if char == "1":
-                    newSeed = self.currSeed[0:i] + "0" + self.currSeed[i+1:]
-                else:
-                    newSeed = self.currSeed[0:i] + "1" + self.currSeed[i+1:]    
-            neighbors.append(newSeed)
-            i+=1
+                neighbor[i]=1
+            neighbors.append(neighbor)
+        #i=0
+        # for char in self.currSeed:
+        #     if i == 0:
+        #         if char == "1":
+        #             newSeed = "0" + self.currSeed[1:]
+        #         else:
+        #             newSeed = "1" + self.currSeed[1:]
+                    
+        #     elif i == len(self.currSeed) - 1:
+        #         if char == "1":
+        #             newSeed = self.currSeed[0:i] + "0"
+        #         else:
+        #             newSeed = self.currSeed[0:i] + "1"
+        #     else:
+        #         if char == "1":
+        #             newSeed = self.currSeed[0:i] + "0" + self.currSeed[i+1:]
+        #         else:
+        #             newSeed = self.currSeed[0:i] + "1" + self.currSeed[i+1:]    
+        #     neighbors.append(newSeed)
+        #     i+=1
         
         bestNeigh = self.findBestNeighbor(neighbors)
         
         if bestNeigh == self.currSeed:
-            print("local maximum reached")
+            print("local maximum reached with dissonance:", self.determineSeedDissonance(bestNeigh))
             print(bestNeigh)
             return bestNeigh
         else:
@@ -136,10 +144,10 @@ class HillClimber(object):
         the best value (or the original state if all neighbors have a worse dissonance). If there are multiple neighbors with the same best value,
         the first one found is chosen"""
         bestNeigh = self.currSeed
-        bestDissonance = self.determineDissonanceFromTune(self.generateCompleteTune(self.currSeed))
+        bestDissonance = self.determineSeedDissonance(self.currSeed)
         
         for neigh in neighbors:
-            dissonance = self.determineDissonanceFromTune(self.generateCompleteTune(neigh))
+            dissonance = self.determineSeedDissonance(neigh)
             if dissonance < bestDissonance:
                 bestNeigh = neigh
                 bestDissonance = dissonance
@@ -147,12 +155,11 @@ class HillClimber(object):
         return bestNeigh
 
 def main():
-    startSeed = ""
+    startSeed = []
     for i in range(0,75):
-        zeroOrOne = random.randint(0,1)
-        startSeed += str(zeroOrOne)
+        startSeed.append(random.randint(0,1))
     
-    hillClimber = HillClimber(startSeed, 50)
+    hillClimber = HillClimber(startSeed, 50, maxRounds = 15)
     hillClimber.run()
     
 if __name__ == "__main__":
